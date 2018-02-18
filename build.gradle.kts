4d875f328248cc3632e66c7d9d44dbb584cefccb
Guido Pio Mariotti <guidopio.mariotti10+dev@gmail.com>
2018-02-18 14:15:51 +0100

    testing bintray deployment

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,15 @@
-import org.jetbrains.dokka.gradle.DokkaTask
-
+import com.jfrog.bintray.gradle.BintrayExtension.PackageConfig
 import java.io.File
 import kotlin.collections.listOf
-import org.gradle.api.tasks.wrapper.Wrapper.DistributionType.ALL
+import org.gradle.api.tasks.wrapper.Wrapper.DistributionType.BIN
 import org.gradle.kotlin.dsl.creating
 import org.gradle.kotlin.dsl.getting
 import org.gradle.kotlin.dsl.kotlin
+import org.jetbrains.dokka.gradle.DokkaTask
 import org.jetbrains.kotlin.gradle.dsl.Coroutines
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+
+// -------------------------- Library Information ----------------------------
 
 group = "com.olx.ps"
 version = "0.1.0"
@@ -40,6 +42,7 @@ apply {
 plugins {
     kotlin("jvm") version "1.2.21"
     id("com.github.ben-manes.versions") version "0.17.0"
+    id("com.jfrog.bintray") version "1.7.3"
     id("io.gitlab.arturbosch.detekt") version ("1.0.0.RC6-2")
     id("jacoco")
     id("java-library")
@@ -65,21 +68,25 @@ val integrationTestCompile: Configuration = configurations["integrationTestCompi
 
 dependencies {
     api(group = "com.bettercloud", name = "vault-java-driver", version = "3.0.0")
-    implementation(kotlin(module = "stdlib-jre8", version = kotlinVersion))
+    api(group = "org.funktionale", name = "funktionale-try", version = "1.2")
+    api(group = "org.slf4j", name = "slf4j-api", version = "1.7.25")
+
+    implementation(kotlin(module = "stdlib-jdk8", version = kotlinVersion))
     implementation(kotlin(module = "reflect", version = kotlinVersion))
-    implementation(group = "com.fasterxml.jackson.module", name = "jackson-module-kotlin", version = "2.9.4.1")
-    implementation(group = "org.funktionale", name = "funktionale-try", version = "1.2")
-    implementation(group = "org.slf4j", name = "slf4j-api", version = "1.7.25")
+    implementation(group = "com.fasterxml.jackson.module", name = "jackson-module-kotlin", version = "2.9.+")
+
     testImplementation(group = "org.jetbrains.kotlinx", name = "kotlinx-coroutines-core", version = "0.22.2")
     testImplementation(group = "junit", name = "junit", version = "4.12")
-    testImplementation(group = "org.mockito", name = "mockito-core", version = "2.13.0")
+    testImplementation(group = "org.mockito", name = "mockito-core", version = "2.15.0")
     testImplementation(group = "com.nhaarman", name = "mockito-kotlin", version = "1.5.0")
     testImplementation(group = "io.kotlintest", name = "kotlintest", version = "2.0.7")
     testImplementation(group = "org.eclipse.jetty", name = "jetty-server", version = "9.4.8.v20171121")
     testImplementation(group = "ch.qos.logback", name = "logback-classic", version = "1.2.3")
+
     integrationTestCompile("org.bouncycastle:bcprov-jdk15on:1.59")
     integrationTestCompile("org.testcontainers:testcontainers:1.6.0")
-    ktlint("com.github.shyiko:ktlint:0.15.0")
+
+    ktlint("com.github.shyiko:ktlint:0.15.1")
 }
 
 // -------------------------- Tasks Setup -----------------------------------
@@ -104,7 +111,7 @@ detekt {
 tasks {
     "wrapper"(Wrapper::class) {
         gradleVersion = "4.5.1"
-        distributionType = ALL
+        distributionType = BIN
     }
 
     "ktlintCheck"(JavaExec::class) {
@@ -169,4 +176,20 @@ publishing {
         artifact(sourceJar)
         artifact(javadocJar)
     }
+}
+
+bintray {
+    user = extra["bintray_user"] as String
+    key = extra["bintray_key"] as String
+
+    setPublications("maven")
+
+    pkg(closureOf<PackageConfig> {
+        repo = base.archivesBaseName
+        name = base.archivesBaseName
+        description = "Kotlin extension of java-vault-driver"
+        vcsUrl = "https://github.com/gmariotti/vault-kotlin-driver.git"
+        setLicenses("Apache-2.0")
+        setLabels("kotlin", "vault", "olx")
+    })
 }
